c3f8eaf21a2a083d6d6aba6e647406179d092954
albicaron <albertoninocaron@gmail.com>
2020-10-22 01:21:44 +0200

    Commit #1

--- olearner/olearner.py
+++ olearner/olearner.py
@@ -36,27 +36,20 @@ def example_data(N=1000, P=10, rng=1):
     X[:, 0:2] = np.asarray([sts.norm.ppf(rv[:, i]) for i in range(2)]).T
 
     # Generate Z
-    und_lin = -0.2 + 0.5*X[:, 0] + 0.7*X[:, 1] + sts.uniform.rvs(size=N)/10
-    pscore = 0.9*(sts.norm.cdf(und_lin))
-    # import seaborn as sns
-    # sns.distplot(pscore, bins=50)
-    # from scipy import stats
-    # stats.describe(pscore)
+    und_lin = -0.2 + 0.5*X[:, 0] + 0.3*X[:, 1]**2 + sts.uniform.rvs(size=N)/10
+    pscore = 0.8*sts.norm.cdf(und_lin)
 
     Z = sts.binom.rvs(1, pscore)
-    # np.bincount(Z)
 
     # Generate UNBALANCED covariates (8 out of 10, Z enters mean)
-    X[:, 2:5] = np.asarray([sts.norm.ppf(rv[:, i], loc=2*Z) for i in range(2, 5)]).T
-    X[:, 5:P] = np.asarray([sts.binom.ppf(rv[:, i], n=1, p=0.3 + 0.3*Z) for i in range(5, P)]).T
+    X[:, 2:5] = np.asarray([sts.norm.ppf(rv[:, i], loc=Z) for i in range(2, 5)]).T
+    X[:, 5:P] = np.asarray([sts.binom.ppf(rv[:, i], n=1, p=0.3 + 0.1*Z) for i in range(5, P)]).T
 
     # Generate Y
-    mu = 6 + 0.5*X[:, 2] + 0.3*(X[:, 3] - 0.5)**2 + 1*X[:, 4]*(X[:, 9] + 1)
-    ITE = 1 + 0.5*X[:, 2]
-    # sns.distplot(mu)
-    # sns.distplot(ITE, bins=30)
+    mu = 6 + 0.3*X[:, 2]**2 + 1*X[:, 3] + 0.5*X[:, 4]*X[:, 8]
+    ITE = 2 + 0.1*X[:, 2]**2 + 0.8*X[:, 8]
 
-    sigma = np.std(ITE)
+    sigma = np.std(ITE)/2
     Y = mu + ITE*Z + sts.norm.rvs(0, sigma, N)
 
     return Y, X, Z, ITE
@@ -74,74 +67,7 @@ def MC_se(x, B):
 
 class OLearner:
     """
-    A O-Learner implementation with sklearn models.
-
-    """
-    def __init__(self, outcome_type="Continuous", pi_model=None, y_model=None):
-        """
-        Class constructor. A model object.
-
-        :pi_model: a sklearn probabilistic classification model (default is LogisticRegression)
-        :y_model: a sklearn regression/classification model (default is LinearRegression)
-        :outcome_type: ["Continuous", "Binary"]
-        """
-        # ~~~~~~~~~~~~~~~~~~~~~~~
-        # ** input arguments
-        # ~~~~~~~~~~~~~~~~~~~~~~~
-        self.outcome_type = outcome_type
-        self.pi_model = pi_model
-        self.y_model = y_model
-
-        if pi_model is None:
-            self.pi_model = LogisticRegression()
-
-        if y_model is None:
-            if self.outcome_type == "Binary":
-                self.y_model = LogisticRegression()
-            else:
-                self.y_model = LinearRegression()
-
-    def fit(self, X, Y, Z, W=None):
-        """
-        Fit Propensity and Outcome models, and returns CATE fit model.
-        X has to be an N x k matrix.
-
-        :X: Covariates N x k numpy array for the Outcome Model
-        :W: Covariates for the propensity score model (default equal to X)
-        :Y: Outcome N x 1 numpy array
-        :Z: Treatment Assignment N x 1 numpy array
-        """
-        # 1) Fit Propensity Model
-        if W is None:
-            W = X
-
-        self.pi_model.fit(W, Z)
-        z_hat = self.pi_model.predict_proba(W)[:, 1]
-
-        # 2) Overlap Weighting step
-        Y_overlap = Y
-        Y_overlap[Z == 1] = Y[Z == 1]*(1 - z_hat[Z == 1])
-        Y_overlap[Z == 0] = Y[Z == 0]*z_hat[Z == 0]
-
-        # 3) Fit Outcome Model
-        self.y_model.fit(X, Y_overlap)
-
-    def predict(self, X):
-        """
-        Predict CATE for set of covariates.
-
-        :X: Covariates N x k_1 numpy array for the Outcome Model
-        :W: Covariates N x k_2 numpy array for the Outcome Model
-        """
-        # Predict PS
-        CATE_est = self.y_model.predict(X)
-
-        return CATE_est
-
-
-class DROLearner:
-    """
-    A doubly-robust implementation of O-Learner with sklearn models.
+    Doubly-robust implementation of O-Learner with sklearn models.
     """
     def __init__(self, outcome_type="Continuous", pi_model=None, y_model=None, g_model=None):
         """
@@ -152,6 +78,9 @@ class DROLearner:
         :y_model: a sklearn regression/classification model for CATE (Default is LinearRegression)
         :outcome_type: ["Continuous", "Binary"]
         """
+        if outcome_type not in ["Continuous", "Binary"]:
+            raise ValueError('Currently supported types are ["Continuous", "Binary"]')
+
         # ~~~~~~~~~~~~~~~~~~~~~~~
         # ** input arguments
         # ~~~~~~~~~~~~~~~~~~~~~~~
@@ -161,17 +90,17 @@ class DROLearner:
         self.y_model = y_model
 
         if self.pi_model is None:
-            self.pi_model = LogisticRegression()
+            self.pi_model = LogisticRegression(solver="lbfgs")
 
         if g_model is None:
             if self.outcome_type == "Binary":
-                self.g_model = LogisticRegression()
+                self.g_model = LogisticRegression(solver="lbfgs")
             else:
                 self.g_model = LinearRegression()
 
         if y_model is None:
             if self.outcome_type == "Binary":
-                self.y_model = LogisticRegression()
+                self.y_model = LogisticRegression(solver="lbfgs")
             else:
                 self.y_model = LinearRegression()
 
@@ -185,31 +114,35 @@ class DROLearner:
         :Y: Outcome N x 1 numpy array
         :Z: Treatment Assignment N x 1 numpy array
         """
+        # Check inputs
+        if len(X.shape) is not 2:
+            raise ValueError('X must be a 2-dimensional N x k numpy array')
+
+        if len(Y) != len(Z) != X.shape[0]:
+            raise ValueError('Y, Z and X must have the same number of rows N')
+
         # 1) Fit raw outcome model (T-Learner)
-        y1_model = self.g_model.fit(X[Z == 1, :], Y[Z == 1])
-        y0_model = self.g_model.fit(X[Z == 0, :], Y[Z == 0])
+        Y_raw_model = self.g_model.fit(np.c_[X, Z], Y)
 
-        y1_fit = y1_model.predict(X[Z == 1, :])
-        y0_fit = y0_model.predict(X[Z == 0, :])
-
-        g_fit = Y
-        g_fit[Z == 1] = y1_fit
-        g_fit[Z == 0] = y0_fit
+        g1_fit = Y_raw_model.predict(np.c_[X, np.ones(X.shape[0])])
+        g0_fit = Y_raw_model.predict(np.c_[X, np.zeros(X.shape[0])])
 
         # 2) Fit Propensity Model
         if W is None:
             W = X
 
         self.pi_model.fit(W, Z)
-        z_hat = self.pi_model.predict_proba(W)[:, 1]
+        z1_hat = self.pi_model.predict_proba(W)[:, 1]
 
-        # 2) Construct DR Overlap-Weighted outcome
-        Y_DR = Y
-        Y_DR[Z == 1] = g_fit[Z == 1] + (Y[Z == 1] - g_fit[Z == 1])*(1 - z_hat[Z == 1])
-        Y_DR[Z == 0] = g_fit[Z == 0] + (Y[Z == 0] - g_fit[Z == 0])*z_hat[Z == 0]
+        # 3) Construct DR Overlap-Weighted outcome
+        Y1_DR = g1_fit
+        Y1_DR[Z == 1] += (Y[Z == 1] - g1_fit[Z == 1])*(1 - z1_hat[Z == 1])
 
-        # 3) fit CATE
-        self.y_model.fit(X, Y_DR)
+        Y0_DR = g0_fit
+        Y0_DR[Z == 0] += (Y[Z == 0] - g0_fit[Z == 0])*z1_hat[Z == 0]
+
+        # 4) fit CATE
+        self.y_model.fit(X, Y1_DR - Y0_DR)
 
     def predict(self, X):
         """
--- olearner/test_model.py
+++ olearner/test_model.py
@@ -2,61 +2,70 @@
 
 # Importing packages
 import pandas as pd
+import numpy as np
 
 from olearner.olearner import PEHE, example_data
-from olearner.olearner import OLearner, DROLearner
+from olearner.olearner import OLearner
 
 from econml.metalearners import SLearner, TLearner, XLearner
 from econml.drlearner import DRLearner
-
-from sklearn.linear_model import LinearRegression
-from sklearn.neural_network import MLPClassifier
-
+from sklearn.linear_model import LinearRegression, LogisticRegression
 
 # Options
-N = 1000
-myY, myX, myZ, ITE = example_data(rng=1)
+N, B = [1000, 200]
 
+models = {"S": np.zeros(N), "T": np.zeros(N), "X": np.zeros(N),
+          "DR": np.zeros(N), "O": np.zeros(N)}
+PEHES = {"S": np.zeros(B), "T": np.zeros(B), "X": np.zeros(B),
+          "DR": np.zeros(B), "O": np.zeros(B)}
 
 # @@@@@@@@@
 # Models
 # @@@@@@@@@
 
-# S-Learner
-myS = SLearner(overall_model=LinearRegression())
-myS.fit(Y=myY, T=myZ, X=myX)
-S_fit = myS.effect(X=myX)
+for i in range(B):
 
-# T-Learner
-myT = TLearner(models=LinearRegression())
-myT.fit(Y=myY, T=myZ, X=myX)
-T_fit = myT.effect(X=myX)
+    # Create data
+    myY, myX, myZ, ITE = example_data(N, rng=i*13)
 
-# X-Learner
-myXL = XLearner(models=LinearRegression(),
-                propensity_model=MLPClassifier(hidden_layer_sizes=(4, 2), max_iter=5000),
-                cate_models=LinearRegression())
-myXL.fit(Y=myY, T=myZ, X=myX)
-XL_fit = myXL.effect(X=myX)
+    # S-Learner
+    myS = SLearner(overall_model=LinearRegression())
+    myS.fit(Y=myY, T=myZ, X=myX)
+    models["S"] = myS.effect(X=myX)
 
-# DR Learner
-myDR = DRLearner(model_propensity=MLPClassifier(hidden_layer_sizes=(4, 2), max_iter=5000),
-                 model_regression=LinearRegression(),
-                 model_final=LinearRegression())
-myDR.fit(Y=myY, T=myZ, X=myX)
-DR_fit = myDR.effect(X=myX)
+    # T-Learner
+    myT = TLearner(models=LinearRegression())
+    myT.fit(Y=myY, T=myZ, X=myX)
+    models["T"] = myT.effect(X=myX)
 
-# O-Learner
-myO = OLearner(pi_model=MLPClassifier(hidden_layer_sizes=(4, 2), max_iter=5000),
-               y_model=LinearRegression())
-myO.fit(myX, myY, myZ)
-O_fit = myO.predict(myX)
+    # X-Learner
+    myXL = XLearner(models=LinearRegression(),
+                    propensity_model=LogisticRegression(solver="lbfgs"),
+                    cate_models=LinearRegression())
+    myXL.fit(Y=myY, T=myZ, X=myX)
+    models["X"] = myXL.effect(X=myX)
+
+    # DR Learner
+    myDR = DRLearner(model_propensity=LogisticRegression(solver="lbfgs"),
+                     model_regression=LinearRegression(),
+                     model_final=LinearRegression())
+    myDR.fit(Y=myY, T=myZ, X=myX)
+    models["DR"] = myDR.effect(X=myX)
+
+    # O-Learner
+    myO = OLearner()
+    myO.fit(myX, myY, myZ)
+    models["O"] = myO.predict(myX)
+
+    # PEHES fill
+    for j in models.keys():
+        PEHES[j][i] = PEHE(ITE, models[j])
 
 
 # Evaluate PEHE for each model
-PEHE_final = {'S-Learner': PEHE(ITE, S_fit), 'T-Learner': PEHE(ITE, T_fit),
-              'X-Learner': PEHE(ITE, XL_fit), 'DR-Learner': PEHE(ITE, DR_fit),
-              'O-Learner': PEHE(ITE, O_fit)}
+PEHE_final = {'S-Learner': np.mean(PEHES["S"]), 'T-Learner': np.mean(PEHES["T"]),
+              'X-Learner': np.mean(PEHES["X"]), 'DR-Learner': np.mean(PEHES["DR"]),
+              'O-Learner': np.mean(PEHES["O"])}
 
 PEHE_final = pd.DataFrame(PEHE_final, index=["PEHE"])
 
